c5f3cceb91ec6d4007398629987fb3455339935a
Kristof Szalontai <5261316+kszalontai@users.noreply.github.com>
2021-10-06 18:56:52 +0200

    updated Request Rocket to 1.3.1

--- Casks/request-rocket.rb
+++ Casks/request-rocket.rb
@@ -1,6 +1,6 @@
 cask 'request-rocket' do
-  version '1.1.2'
-  sha256 '1e0fa76f8ac7305df3327252d37997e38d269a484882fbd0ccf277426397ff3c'
+  version "1.3.1"
+  sha256 "75a48a68b4092c054ef11b8906635faae310de6be3d7de5a1390886e04e5ff49"
 
   url "https://github.com/emartech/request-rocket/releases/download/#{version}/Request.Rocket-#{version}.dmg"
   appcast 'https://github.com/emartech/request-rocket/releases.atom'
