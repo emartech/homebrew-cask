6221a576595e15afb969804cf52a2717b35a18fd
Kristof Szalontai <5261316+kszalontai@users.noreply.github.com>
2021-11-05 11:51:30 +0100

    updated Request Rocket to 1.3.2

--- Casks/request-rocket.rb
+++ Casks/request-rocket.rb
@@ -1,11 +1,11 @@
-cask 'request-rocket' do
-  version "1.3.1"
-  sha256 "75a48a68b4092c054ef11b8906635faae310de6be3d7de5a1390886e04e5ff49"
+cask "request-rocket" do
+  version "1.3.2"
+  sha256 "1a630b95190ff00269dbbef484b75cb97599a78877d90f049c7b0817c7188efa"
 
   url "https://github.com/emartech/request-rocket/releases/download/#{version}/Request.Rocket-#{version}.dmg"
-  appcast 'https://github.com/emartech/request-rocket/releases.atom'
-  name 'Request Rocket'
-  homepage 'https://github.com/emartech/request-rocket'
+  appcast "https://github.com/emartech/request-rocket/releases.atom"
+  name "Request Rocket"
+  homepage "https://github.com/emartech/request-rocket"
 
-  app 'Request Rocket.app'
+  app "Request Rocket.app"
 end
