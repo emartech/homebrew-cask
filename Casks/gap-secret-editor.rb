3c5d9fc62745a168656a6950b239270aaeb89f4c
Istvan Demeter <demeter.istvan@gmail.com>
2023-06-27 15:05:51 +0200

    new version of gap-secret-editor
    Co-authored-by: Miklos Bazso <miklos.bazso@emarsys.com>

--- Casks/gap-secret-editor.rb
+++ Casks/gap-secret-editor.rb
@@ -1,6 +1,6 @@
 cask "gap-secret-editor" do
-  version "1.15.0"
-  sha256 "8f19e9cfbbbbd8ff3956caa458a9227e9c509c2580ef04d8e6293847b1d9be6f"
+  version "1.15.2"
+  sha256 "fdc777d08dfa3826ff8513136dde79c1eb863abd77cb4c617c95d5e2e164984a"
 
   url "https://github.com/emartech/gap-secret-editor/releases/download/v#{version}/GAP-Secret-Editor-#{version}.dmg"
   appcast "https://github.com/emartech/gap-secret-editor/releases.atom"
